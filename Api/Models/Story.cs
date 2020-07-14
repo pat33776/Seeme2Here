a3810018f60df370372337d8ce36fac8703e1675
pat33776 <ngee27@yahoo.com>
2020-07-14 13:14:01 -0400

    Create Story.cs

---
+++ Api/Models/Story.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngAPI.Models
+{
+    public class Story
+    {
+        public string By { get; set; }
+        public int Descendants { get; set; }
+        public int Id { get; set; }
+        public int Time { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string Url { get; set; }
+    }
+}
