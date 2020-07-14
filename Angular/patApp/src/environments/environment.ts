3f93a7013f92c6bf72ccf264925d2a994b7d7c4f
pat33776 <ngee27@yahoo.com>
2020-07-14 16:00:20 -0400

    Create environment.ts

---
+++ Angular/patApp/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  backend: {
+    topStoryBaseURL: "https://hacker-news.firebaseio.com/v0/topstories",
+    itemBaseURL: "https://hacker-news.firebaseio.com/v0/item",
+    authenticateURL: "http://localhost:4000",
+    angApiURL: "https://localhost:44353"
+  }
+};
